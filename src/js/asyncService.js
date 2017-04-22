5d7b4170a0a49aa5c9af11d81bdb87b75bfc6d2b
shoheiono <wa2.a.nek0ch+git@gmail.com>
2017-04-22 13:46:36 +0900

    [add] カーソルを合わせるとリストが出る

--- src/js/asyncService.js
+++ src/js/asyncService.js
@@ -19,13 +19,114 @@ xhr.open = (callback, url) => {
 
 function getFamilyAppData() {
 	return {
-		total: 13,
 		items: [
 			{
-				name: 'LINE HERE',
-				logo: 'https://d.line‐scdn.net/stf/line‐lp/family/it/lp_here_appicon_190x190.png'
+				"id" : 0,
+				"name": "B612",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/b612_190.png"
 			},
+			{
+				"id" : 1,
+				"name": "LOOKS",
+				"logo": "https://d.line-scdn.net/stf/line-lp/line_looks_190x190.png"
+			},
+			{
+				"id" : 2,
+				"name": "LINE MAN",
+				"logo": "https://d.line-scdn.net/stf/line-lp/line_android_190x190_1111.png"
+			},
+			{
+				"id" : 3,
+				"name": "LINE HEREWeb site",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/lp_here_appicon_190x190.png"
+			},
+			{
+				"id" : 4,
+				"name": "Emoji LINE",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/190X190_line_me.png"
+			},
+			{
+				"id" : 5,
+				"name": "LINE MomentsWeb site",
+				"logo": "https://d.line-scdn.net/stf/line-lp/iOS_appcion_190-190.png"
+			},
+			{
+				"id" : 6,
+				"name": "LINE@Web site",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/lineat_96.png"
+			},
+			{
+				"id" : 7,
+				"name": "LINE TV",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/linelogo_96x96.png"
+			},
+			{
+				"id" : 8,
+				"name": "LINE CameraWeb site",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/linecamera_icon_96.png"
+			},
+			{
+				"id" : 9,
+				"name": "LINE Dictionary English-Indonesian Web site",
+				"logo": "https://d.line-scdn.net/stf/line-lp/favicon_enid_96.png"
+			},
+			{
+				"id" : 10,
+				"name": "LINE DECO",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/LINE-DECO_96.jpg"
+			},
+			{
+				"id" : 11,
+				"name": "LINE PLAYWeb site",
+				"logo": "https://d.line-scdn.net/stf/line-lp/FreeCoin_CHERRY_96_0314.png"
+			},
+			{
+				"id" : 12,
+				"name": "LINE SnapMovie",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/SnapMovie_96x96.png"
+			},
+			{
+				"id" : 13,
+				"name": "LINE Antivirus",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/LP_Family_icon_96_LINE-Antivirus.jpg"
+			},
+			{
+				"id" : 14,
+				"name": "LINE Brush",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/LINEbrush_icon_96.png"
+			},
+			{
+				"id" : 15,
+				"name": "LINE Greeting Card",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/LP_Family_icon_96_LINE-Card.jpg"
+			},
+			{
+				"id" : 16,
+				"name": "LINE Tools",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/LP_Family_icon_96_LINE-Tools.jpg"
+			},
+			{
+				"id" : 17,
+				"name": "LINE Dictionary English-Thai",
+				"logo": "https://d.line-scdn.net/stf/line-lp/favicon_enth_96.png"
+			},
+			{
+				"id" : 18,
+				"name": "LINE Dictionary Chinese-English",
+				"logo": "https://d.line-scdn.net/stf/line-lp/favicon_cnen_96.png"
+			},
+			{
+				"id" : 19,
+				"name": "LINE SHOP",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/lineshop_96.png"
+			},
+			{
+				"id" : 20,
+				"name": "Popcorn Buzz",
+				"logo": "https://d.line-scdn.net/stf/line-lp/family/en/lp_pop_appicon_96x96.png"
+			}
 		]
+
 	};
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,83 @@ import { asyncConnect } from './asyncService';
 const CLASS_LIST_HIDE = 'search-list--hide';
 let input;
 let searchList;
+let appData = {};
 
+/**
+ * 要素の設定
+ */
 function setElement() {
 	input = document.getElementById('input');
 	searchList = document.getElementById('list');
 }
 
+/**
+ * イベント設定
+ */
+function setEvent() {
+	input.addEventListener('focus', focusIn, false);
+	input.addEventListener('focusout', focusOut, false);
+}
+
+/**
+ * 入力フォームにフォーカスしたときのイベント
+ * @param {MouseEvent} e 
+ */
+function focusIn(e) {
+	searchList.classList.remove(CLASS_LIST_HIDE);
+}
+
+/**
+ * 入力フォームのフォーカスを外したしたときのイベント
+ * @param {MouseEvent} e 
+ */
+function focusOut(e) {
+	searchList.classList.add(CLASS_LIST_HIDE);
+}
+
+function listItemClick(e) {
+	const target = e.currentTarget;
+	const id = target.dataset['id'];
+}
+
+function createList(items) {
+	for (let i = 0, len = items.length; i < len; ++i) {
+		const item = items[i];
+		const itemElem = document.createElement('li');
+		const itemImgElem = document.createElement('img');
+		const itemNameElem = document.createElement('span');
+
+		itemElem.classList.add('search-list__item');
+		itemElem.dataset['id'] = data.id;
+		itemElem.addEventListener('click', listItemClick, false);
+
+		itemImgElem.classList.add('search-list__item-img');
+		itemImgElem.src = item.logo;
+		itemImgElem.width = 40;
+		itemImgElem.height = 40;
+
+		itemNameElem.classList.add('search-list__item-name');
+		itemNameElem.innerText = itemNameElem.title = item.name;
+
+		itemElem.appendChild(itemImgElem);
+		itemElem.appendChild(itemNameElem);
+		searchList.appendChild(itemElem);
+	}
+}
+
+/**
+ * 初期化
+ */
 function initialize() {
 	setElement();
 	asyncConnect(
 		'https://line.me/en/family-apps',
 		(data) => {
+			appData = data;
 			console.log('connected');
 			console.log(data);
+			createList(appData.items);
+			setEvent();
 		}
 	);
 }
