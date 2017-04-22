bcdcd80e1ec975756aaa87269d463c6f8d6158d7
shoheiono <wa2.a.nek0ch+git@gmail.com>
2017-04-22 21:07:29 +0900

    [update] ソート時にidも考慮する

--- src/js/script.js
+++ src/js/script.js
@@ -240,6 +240,11 @@ function createList(items) {
 		} else if (aNum > bNum) {
 			return -1;
 		} else {
+			if (a.id > b.id) {
+				return 1;
+			} else if (a.id < b.id) {
+				return -1;
+			}
 			return 0;
 		}
 
